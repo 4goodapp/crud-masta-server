1f406503f17ac796f5d62b79e4b1dda105df4b87
Anantya Wibowo <anantyaw@gmail.com>
2014-04-16 14:38:13 +0700

    add sample data for person and give variable name for each master data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,6 +3,8 @@ import com.masta.crud.Country
 import com.masta.crud.EducationType
 import com.masta.crud.Gender
 import com.masta.crud.Hobby
+import com.masta.crud.Person
+import com.masta.crud.PersonEducation
 import com.masta.crud.Province
 import com.masta.crud.Religion
 
@@ -27,22 +29,43 @@ class BootStrap {
 		def cityJember = new City(name:"Jember",province:provinceJatim).save(failOnError:true)
 		def cityGresik = new City(name:"Gresik",province:provinceJatim).save(failOnError:true)
 		
-		new EducationType(name: "SD").save(failOnError:true)
-		new EducationType(name: "SMP").save(failOnError:true)
-		new EducationType(name: "SMA").save(failOnError:true)
-		new EducationType(name: "S1").save(failOnError:true)
+		def sd = new EducationType(name: "SD").save(failOnError:true)
+		def smp = new EducationType(name: "SMP").save(failOnError:true)
+		def sma = new EducationType(name: "SMA").save(failOnError:true)
+		def s1 = new EducationType(name: "S1").save(failOnError:true)
 		
-		new Gender(code: "M",  name: "Male").save(failOnError:true)
-		new Gender(code: "F",  name: "Female").save(failOnError:true)
+		def male = new Gender(code: "M",  name: "Male").save(failOnError:true)
+		def female = new Gender(code: "F",  name: "Female").save(failOnError:true)
 		
-		new Hobby(name: "Computer").save(failOnError:true)
-		new Hobby(name: "Music").save(failOnError:true)
-		new Hobby(name: "Game").save(failOnError:true)
+		def computer = new Hobby(name: "Computer").save(failOnError:true)
+		def music = new Hobby(name: "Music").save(failOnError:true)
+		def game = new Hobby(name: "Game").save(failOnError:true)
 		
-		new Religion(name: "Islam").save(failOnError:true)
-		new Religion(name: "Kristen").save(failOnError:true)
-		new Religion(name: "Buddha").save(failOnError:true)
-		new Religion(name: "Hindu").save(failOnError:true)
+		def islam = new Religion(name: "Islam").save(failOnError:true)
+		def kristen = new Religion(name: "Kristen").save(failOnError:true)
+		def buddha = new Religion(name: "Buddha").save(failOnError:true)
+		def hindu = new Religion(name: "Hindu").save(failOnError:true)
+		
+		def person = new Person()
+		person.name="Arkenoid"
+		person.password="mjtqs123"
+		person.birthDay=new Date()
+		person.birthCity=citySurabaya
+		person.gender=male
+		person.description="Saya orang yang sangat"
+		person.religion=islam
+		person.hobbies=[computer,music]
+		person.save(failOnError:true)
+		
+		def education1 = new PersonEducation(
+			name: "SDN 218", 
+			educationType: sd,
+			person:person).save(failOnError:true)
+		def education2 = new PersonEducation(
+			name: "SMPN 19", 
+			educationType: smp,
+			person:person).save(failOnError:true)
+		
     }
     def destroy = {
     }
